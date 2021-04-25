8736dad86359814500eadb49052c7788c4a58745
Joey Bayes <joey.bayes@uscca.com>
2021-04-25 16:00:26 -0400

    Add some results where the part the user searched is bold.

--- src/pages/docs/docs.tsx
+++ src/pages/docs/docs.tsx
@@ -1,11 +1,12 @@
-import { App, ISeoService, Page, ParseJsx, Fragment, Route, State } from 'fyord';
+import { App, ISeoService, Page, ParseJsx, Fragment, Route, State, RawHtml } from 'fyord';
 import { Queryable } from 'tsbase/Collections/Queryable';
 import { Strings } from 'tsbase/Functions/Strings';
 import { Header } from '../../components/header/header';
 import { SnippetComponent } from '../../components/module';
-import { Docs as docsData } from '../../docs';
+import { Docs as docsData, Documentation } from '../../docs';
 import styles from './docs.module.scss';
 
+const searchTermParamKey = 'search';
 const searchInputIdKey = 'searchInput';
 const quickStart = `npx fyord-cli new NewFyordApp
 cd NewFyordApp
@@ -26,7 +27,12 @@ export class Docs extends Page {
     super(seoService, app, windowDocument);
   }
 
-  Template = async () => {
+  // eslint-disable-next-line complexity
+  Template = async (route?: Route) => {
+    if (!this.searchTerm && route?.queryParams.has(searchTermParamKey)) {
+      this.searchTerm = route.queryParams.get(searchTermParamKey) as string;
+    }
+
     const searchTermEntered = this.searchTerm.trim().length >= 3;
     const searchResults = searchTermEntered ? Queryable.From(docsData).Search(this.searchTerm, 3).ToArray() : [];
 
@@ -51,26 +57,37 @@ export class Docs extends Page {
           <>
             {searchResults.length > 0 ?
               <>
-                <p>{searchResults.length} results found</p>
-                <ul>
-                  {searchResults.map(r => <li>
-                    <a href={`/docs/${r.Name.toLocaleLowerCase()}`}>
-                      <p>{r.Name}</p>
-                      <p>{r.Description}</p>
-                    </a>
-                  </li>)}
+                <p>{searchResults.length} result{searchResults.length > 1 ? 's' : Strings.Empty} found</p>
+                <ul class={styles.resultList}>
+                  {await Promise.all(searchResults.map(async r =>
+                    <li>
+                      <a href={`/docs/${r.Name.toLocaleLowerCase()}`}>
+                        <h3>{r.Name}</h3>
+                        {await new RawHtml(this.getRelevantDescription(r)).Render()}
+                      </a>
+                    </li>))}
                 </ul>
               </> :
               <p>No results for "{this.searchTerm}" found.</p>}
           </> :
-          <>
+          <div class={styles.gettingStarted}>
             <h2>Getting Started</h2>
             <p>Scaffold a new Fyord app with our CLI and dive right in!</p>
 
             {await new SnippetComponent(quickStart).Render()}
-          </>
+          </div>
         }
       </section>
     </div>;
+  }
+
+  private getRelevantDescription(documentation: Documentation): string {
+    const stringifiedDocumentation = JSON.stringify(documentation).replace(/[^a-zA-Z0-9 ]/g, Strings.Empty);
+
+    if (stringifiedDocumentation.includes(this.searchTerm)) {
+      return `<p>...<b>${this.searchTerm}</b>${stringifiedDocumentation.split(this.searchTerm)[1].slice(0, 50)}...</p>`;
+    } else {
+      return documentation.Description;
+    }
   }
 }
