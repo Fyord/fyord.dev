ad0f8ec51ef26f28b7d154c2da75fbfa8a55ab16
Joey Bayes <joey.bayes@uscca.com>
2021-05-17 09:45:07 -0400

    Upgrade fyord to 0.0.39.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,11 @@
-import { App, Environments, Route, Asap } from 'fyord';
+import { App, Environments } from 'fyord';
 import { defaultLayout } from './core/module';
 
 import './styles/base.scss';
 
-function scrollToTopOrHash(route: Route | undefined) {
-  const hashElementId = route?.hashParams[0];
-  if (hashElementId) {
-    const hashElement = document.getElementById(hashElementId);
-    if (hashElement) {
-      window.scrollTo(0, hashElement.offsetTop > 50 ? hashElement.offsetTop - 50 : hashElement.offsetTop);
-    }
-
-  } else {
-    window.scroll(0, 0);
-  }
-}
-
 (async () => {
   const app = App.Instance(process.env.NODE_ENV || Environments.Production);
   await app.Start(defaultLayout);
-
-  app.Router.Route.Subscribe(async (route?: Route) => {
-    Asap(() => {
-      scrollToTopOrHash(route);
-    });
-  });
 
   window['app'] = app;
 })();
